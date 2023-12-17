0106acfcd75c86afc1cea13b62c30425f3c5d64f
UO264246 <59111176+UO264246@users.noreply.github.com>
2023-12-17 13:57:51 +0100

    WIP login/logout/register for users

--- fitness/urls.py
+++ fitness/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('activities/', views.activity_list, name='activity_list'),
     path('diet/', views.diet_log, name='diet_log'),
-    # Add more URL patterns as needed
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='fitness/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- fitness/views.py
+++ fitness/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import FitnessActivity, DietaryLog
+from .forms import UserRegisterForm
+
 
 def activity_list(request):
     activities = FitnessActivity.objects.filter(user=request.user)
@@ -8,3 +10,16 @@ def activity_list(request):
 def diet_log(request):
     diet_logs = DietaryLog.objects.filter(user=request.user)
     return render(request, 'fitness/diet_log.html', {'diet_logs': diet_logs})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'fitness/register.html', {'form': form})
+
+def home(request):
+    return render(request, 'fitness/home.html')  # Render a template for the home page
